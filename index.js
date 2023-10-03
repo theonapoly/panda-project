d34d57ccfe29c26d44dc4c5e1e3fdf304ea31fdf
THEO NAPOLY <THEO.NAPOLY@gmail.com>
2023-10-03 20:34:01 +0200

    create carrousel

--- index.js
+++ index.js
@@ -17,3 +17,36 @@ containers.forEach((parentContainer) => {
     current.textContent = current.textContent.includes("+") ? "-" : "+";
   });
 });
+
+// Carrousel
+document.body.onload = function () {
+  const nbrPics = 7;
+  let position = 0;
+  const container = document.querySelector(".container-carrousel");
+  const btnLeft = document.getElementById("btn-left");
+  const btnRight = document.getElementById("btn-right");
+
+  container.style.width = 400 * nbrPics + "px";
+  for (let i = 1; i <= nbrPics; i++) {
+    const divPics = document.createElement("div");
+    divPics.className = "photo";
+    divPics.style.backgroundImage = `url('/assets/photos/photo-panda-${i}.jpg')`;
+    container.appendChild(divPics);
+  }
+
+  btnLeft.addEventListener("click", function () {
+    if (position > 0) {
+      position--;
+      container.style.transform = `translateX(-${position * 400}px)`;
+      container.style.transition = "all 0.5s ease";
+    }
+  });
+
+  btnRight.addEventListener("click", function () {
+    if (position < nbrPics - 1) {
+      position++;
+      container.style.transform = `translateX(-${position * 400}px)`;
+      container.style.transition = "all 0.5s ease";
+    }
+  });
+};
