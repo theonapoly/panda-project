942a17da4da3eda69f55f302098987ec1fc7923e
THEO NAPOLY <THEO.NAPOLY@gmail.com>
2023-10-05 09:25:19 +0200

    efect text JS

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Bouton "voir plus"
+// Bouton "voir plus" -----------------------
 const containers = document.querySelectorAll(".section");
 
 containers.forEach((parentContainer) => {
@@ -18,7 +18,7 @@ containers.forEach((parentContainer) => {
   });
 });
 
-// Carrousel
+// Carrousel -----------------------
 document.body.onload = function () {
   const nbrPics = 7;
   let position = 0;
@@ -26,7 +26,7 @@ document.body.onload = function () {
   const btnLeft = document.getElementById("btn-left");
   const btnRight = document.getElementById("btn-right");
 
-  container.style.width = 400 * nbrPics + "px";
+  container.style.width = 100 * nbrPics + "vw";
   for (let i = 1; i <= nbrPics; i++) {
     const divPics = document.createElement("div");
     divPics.className = "photo";
@@ -37,7 +37,7 @@ document.body.onload = function () {
   btnLeft.addEventListener("click", function () {
     if (position > 0) {
       position--;
-      container.style.transform = `translateX(-${position * 400}px)`;
+      container.style.transform = `translateX(-${position * 100}vw)`;
       container.style.transition = "all 0.5s ease";
     }
   });
@@ -45,8 +45,71 @@ document.body.onload = function () {
   btnRight.addEventListener("click", function () {
     if (position < nbrPics - 1) {
       position++;
-      container.style.transform = `translateX(-${position * 400}px)`;
+      container.style.transform = `translateX(-${position * 100}vw)`;
       container.style.transition = "all 0.5s ease";
     }
   });
 };
+
+// MagicScroll -----------------------
+// let controller = new ScrollMagic.Controller();
+
+// let scene = new ScrollMagic.Scene({
+//   triggerElement: ".section-2",
+// })
+
+//   .setClassToggle(".section-2", "show")
+//   .addTo(controller);
+
+// const body = document.getElementsByTagName("body");
+
+// Effet Parallax -----------------------
+const sectionOne = document.querySelector(".section-1");
+const iconsInfos = document.querySelector(".icons-infos");
+
+const sectionTwo = document.querySelector(".section-2");
+const strongWhite = document.querySelector(".strong-text-white");
+
+const sectionThree = document.querySelector(".section-3");
+const carrouselPart = document.querySelector(".carrousel");
+
+const sectionFour = document.querySelector(".section-4");
+const animeTwo = document.querySelector(".anim-2");
+
+const sectionFive = document.querySelector(".section-5");
+const animeThree = document.querySelector(".nextanim");
+
+const sectionSix = document.querySelector(".section-6");
+const strongBlack = document.querySelector(".strong-text-black");
+
+let offTop, offBot;
+
+function parallax(elemChange, elemNext) {
+  let offTop = elemChange.offsetTop - 700;
+  let offBot = elemNext.offsetTop - 0.3 * window.innerHeight;
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY >= offTop && window.scrollY < offBot) {
+      elemChange.classList.add("show");
+      const classShow = document.querySelector(".show");
+      classShow.style.opacity = "1";
+      classShow.style.transition = "2s";
+    } else {
+      elemChange.classList.remove("show");
+      elemChange.style.opacity = "0";
+    }
+    offTop = elemChange.offsetTop - 700;
+    offBot = elemNext.offsetTop - 0.3 * window.innerHeight;
+  });
+}
+
+parallax(sectionOne, iconsInfos);
+parallax(sectionTwo, strongWhite);
+parallax(sectionThree, carrouselPart);
+parallax(sectionFour, animeTwo);
+parallax(sectionFive, animeThree);
+parallax(sectionSix, strongBlack);
+
+// window.addEventListener("click", (event) => {
+//   console.log(event);
+// });
